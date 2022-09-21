89303e724b246ae917a59985de0f65ba703ff6d5
wantedcookieNod <111057867+wantedcookieNod@users.noreply.github.com>
2022-09-21 16:26:43 +0430

    Add files via upload

---
+++ Library prog/Library prog/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Library_prog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Sort = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "Humanbody",
+            "Dinosours",
+            "Facts of old people",
+            "Stories for kids",
+            "Judy became famous",
+            "504 Words of English",
+            "Space around us",
+            "How to play piano",
+            "Samsam ST.",
+            "Beyer",
+            "Family and friends Starter",
+            "Family and friends 1",
+            "Family and friends 2",
+            "Family and friends 3",
+            "Family and friends 4",
+            "Family and friends 5",
+            "Family and friends 6",
+            "Rise and Shine B1",
+            "Rise and Shine B2",
+            "Rise and Shine B3",
+            "Rise and Shine E1",
+            "Rise and Shine E2",
+            "Rise and Shine E3",
+            "Rise and Shine E4",
+            "Rise and Shine P1",
+            "Rise and Shine P2",
+            "Rise and Shine P3",
+            "Rise and Shine P4",
+            "Rise and Shine I1",
+            "Rise and Shine I2",
+            "Rise and Shine I3 ",
+            "Rise and Shine I4",
+            "Rise and Shine High 1",
+            "Rise and Shine High 2",
+            "Rise and Shine High 3",
+            "Rise and Shine AD 1",
+            "Rise and Shine AD 2",
+            "Rise and Shine AD 3",
+            "Rise and Shine AD 4",
+            "Rise and Shine AD 5",
+            "School for Good and Evil",
+            "Sea Dragon ",
+            "How to draw Vol.1",
+            "How to draw Vol.2",
+            "How to draw Vol.3",
+            "How to draw Vol.4",
+            "How to draw Vol.5"});
+            this.listBox1.Location = new System.Drawing.Point(22, 302);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(231, 238);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox1.Location = new System.Drawing.Point(22, 251);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 45);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(305, 245);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(147, 51);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Visible = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Sort
+            // 
+            this.Sort.Location = new System.Drawing.Point(305, 302);
+            this.Sort.Name = "Sort";
+            this.Sort.Size = new System.Drawing.Size(147, 55);
+            this.Sort.TabIndex = 3;
+            this.Sort.Text = "Sort";
+            this.Sort.UseVisualStyleBackColor = true;
+            this.Sort.Visible = false;
+            this.Sort.Click += new System.EventHandler(this.Sort_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Remove.Location = new System.Drawing.Point(305, 365);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(147, 54);
+            this.Remove.TabIndex = 4;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Visible = false;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(305, 425);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(147, 54);
+            this.Clear.TabIndex = 5;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Visible = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(305, 485);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(147, 55);
+            this.Search.TabIndex = 6;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Visible = false;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(66, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(66, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox2.Location = new System.Drawing.Point(185, 42);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 40);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox3.Location = new System.Drawing.Point(185, 87);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(152, 40);
+            this.textBox3.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 552);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Sort);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Library";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Sort;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Library prog/Library prog/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_prog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            MessageBox.Show("Succesfully added to list.");
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == listBox1.Text)
+                listBox1.Items.Clear();
+            listBox1.Text = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            string b;
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToString(textBox2.Text);
+            if (a == 1384 && b == "farnood")
+            {
+                listBox1.Visible = true;
+                textBox1.Visible = true;
+                Add.Visible = true;
+                Sort.Visible = true;
+                Search.Visible = true;
+                Clear.Visible = true;
+                Remove.Visible = true;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                button1.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+            }
+            else
+                MessageBox.Show("Wrong Password/Username");
+        }
+    }
+}
